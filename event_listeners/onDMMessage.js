a7f69746f22af2807bda75fc92f2030ff1aba2c6
Kaaiitoo <paolo.santos_12@yahoo.com.ph>
2020-08-07 03:46:22 +0800

    added complaint system and updated server boost listener

---
+++ event_listeners/onDMMessage.js
@@ -0,0 +1,53 @@
+/**
+ * @file DM listener for Q-Den complaints
+ */
+const { TextChannel, MessageEmbed } = require('discord.js');
+const colors = require('../colors.json');
+
+ module.exports = async bot =>{
+     bot.on('message', message => {
+         let text = message.content.toLowerCase();
+
+         if (text === 'complaint') {
+             
+            if (message.channel instanceof TextChannel) {
+                return;
+            }
+
+            const guild = bot.guilds.cache.get('690499818489118722');
+            const channel = guild.channels.cache.find(c => c.id === '692411870417780786');
+
+            message.reply("Hello there, please type your complaint now.");
+            const filter = m => m.author.id !== bot.user.id;
+
+            message.channel.awaitMessages(filter, { max:1 }).then(collected => {
+                if (collected){
+                    message.reply('Thank you for your complaint! Have a nice day!');
+                    const embed = new MessageEmbed()
+                        .setTitle("**New Complaint!**")
+                        .addFields(
+                            {
+                                name: "──────────────── ∘°❉°∘ ─────────────────",
+                                value: `${collected.first().content}`,
+                                inline: true
+                            },
+                            { 
+                                name: "\u2800",
+                                value: "\u2800",
+                                inline: true
+                            },
+                            {
+                                name: "──────────────── °∘❉∘° ─────────────────",
+                                value: "\u2800"
+                            }
+                        )
+                        .setColor(colors.Turquoise)
+                        .setFooter(`${bot.user.username} | By MahoMuri`, bot.user.displayAvatarURL())
+                        .setTimestamp();
+                    channel.send(embed);
+                }
+            });
+
+         }
+     });
+ };
