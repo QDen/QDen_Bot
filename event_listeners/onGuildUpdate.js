d83f0af9830aa097a0c92a5ce45370291ac7724d
Kaaiitoo <paolo.santos_12@yahoo.com.ph>
2020-08-06 19:49:43 +0800

    added serverboost notification

--- event_listeners/onGuildUpdate.js
+++ event_listeners/onGuildUpdate.js
@@ -7,43 +7,41 @@ const colors = require('../colors.json');
 
  module.exports = async bot =>{
 
-    // bot.on('guildUpdate' , async (oldGuild, newGuild) =>{
-    //     const channel = bot.channels.cache.get('740615971038822500');
-    //     const testChannel = bot.channels.cache.get('694568193292763157');
-    //     const basic = oldGuild.premiumSubscriptionCount;
-    //     const premium = newGuild.premiumSubscriptionCount;
-    //     const date = new Date(Date.now());
+    bot.on('guildUpdate' , async (oldGuild, newGuild) =>{
+        const channel = bot.channels.cache.get('740615971038822500');
+        const testChannel = bot.channels.cache.get('694568193292763157');
+        const basic = oldGuild.premiumSubscriptionCount;
+        const premium = newGuild.premiumSubscriptionCount;
+        const date = new Date(Date.now());
 
-    //     if (oldGuild.name && oldGuild.name !== newGuild.name) {
-    //         const subscriber = newGuild.members.cache
-    //         .map(m => m.premiumSince);
-    //         const member = newGuild.members.cache.filter(m => m.premiumSince);
-            
-            
-    //         const dates = subscriber.filter(function(m) {
-    //             return m !== null;
-    //         });
+        if (oldGuild.name && oldGuild.name !== newGuild.name) {
 
-    //         let subscribers = [];
+            const member = newGuild.members.cache
+            .map(m => {
+                let subscribers = {};
+                let username = m.user.username;
+                let premium = m.premiumSince;
+                subscribers = {
+                    username: username,
+                    premium: premium
+                };
+                return subscribers;
+            })
+            .filter(function(m) {
+                return m.premium !== null;
+            }).sort((a,b) => b.premium - a.premium);
 
-    //         for (let i = 0; i < dates.length; i++) {
-    //             if (dates.length === 0) {
-    //                 subscribers =`${i + 1}. ${new Date(dates[i])}`;
-    //             } else {
-    //                 subscribers = subscribers + `${i + 1}. ${new Date(dates[i])}\n`;
-    //             }
-    //         }
+            if (member[0].premium.toLocaleDateString() === date.toLocaleDateString()) {
+                const embed = new MessageEmbed()
+                    .setTitle('**New Server Boost!**')
+                    .setDescription(`Cheers! ${member[0].username} has just boosted the server!`)
+                    .setColor(colors.Blue);
+                channel.send(embed);
+                
+            }
 
-    //         console.log(member.filter(function(member) {
-    //             return member.user.username;
-    //         }));
 
-    //         // const embed = new MessageEmbed()
-    //         //     .setTitle('**New Server Boost!**')
-    //         //     .setDescription(`Cheers! ${member} has just boosted the server!`)
-    //         //     .setColor(colors.Blue);
-    //         // channel.send(embed);
-    //     }
+        }
 
-    // });
+    });
  };
