f46bcd9fa62c4392fc31833af4efdaed9263e927
Kaaiitoo <paolo.santos_12@yahoo.com.ph>
2021-01-20 01:47:10 +0800

    updated npm packages, started on setup command

--- src/commands/staffs/test.js
+++ src/commands/staffs/test.js
@@ -1,5 +1,5 @@
-const { JWT } = require("google-auth-library");
-const { google } = require("googleapis");
+// const { JWT } = require("google-auth-library");
+// const { google } = require("googleapis");
 
 module.exports = {
     name: "test",
@@ -11,27 +11,30 @@ module.exports = {
         // https://docs.google.com/spreadsheets/d/15PUfYN8vta0bdWeOydwdk3HEPFZ07wHcl6Otbg04emk/edit#gid=1750226729
         // TODO: Implement this to tournamnet sheets
 
-        if (args.length < 1) {
-            message.channel.send("**❌ Please provide a valid URL**");
-        } else {
-            const client = new JWT({
-                email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-                key: process.env.GOOGLE_PRIVATE_KEY,
-                scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-                subject: null,
-            });
-            const sheets = google.sheets({
-                version: "v4",
-                auth: client,
-            });
-            const url = new URL(args.join(""));
-            const regex = /([^spreadsheets?:\/\s])([^\/\s]+)([^\/edit\s])/g;
-            const spreadsheetID = url.pathname.match(regex).join("");
-            const res = await sheets.spreadsheets.values.get({
-                spreadsheetId: spreadsheetID,
-                range: "A1:Z",
-            });
-            console.log(res.data.values);
-        }
+        // if (args.length < 1) {
+        //     message.channel.send("**❌ Please provide a valid URL**");
+        // } else {
+        //     const client = new JWT({
+        //         email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        //         key: process.env.GOOGLE_PRIVATE_KEY,
+        //         scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+        //         subject: null,
+        //     });
+        //     const sheets = google.sheets({
+        //         version: "v4",
+        //         auth: client,
+        //     });
+        //     const url = new URL(args.join(""));
+        //     const regex = /([^spreadsheets?:\/\s])([^\/\s]+)([^\/edit\s])/g;
+        //     const spreadsheetID = url.pathname.match(regex).join("");
+        //     const res = await sheets.spreadsheets.values.get({
+        //         spreadsheetId: spreadsheetID,
+        //         range: "A1:Z",
+        //     });
+        //     console.log(res.data.values);
+        // }
+
+        const template = await bot.fetchGuildTemplate(args.join(""));
+        console.log(template);
     },
 };
--- src/commands/tournament/setup.js
+++ src/commands/tournament/setup.js
@@ -1,3 +1,9 @@
+const { MessageEmbed } = require("discord.js");
+const { JWT } = require("google-auth-library");
+const { google } = require("googleapis");
+
+const colors = require("../../utils/colors.json");
+
 module.exports = {
     name: "setup",
     aliases: [],
@@ -5,6 +11,38 @@ module.exports = {
     description: "Creates a Trounament environment for a specific game",
     usage: ["`-<command | alias> `"],
     async run(bot, message, args) {
-        console.log(args);
+        if (!message.member.hasPermission("MANAGE_GUILD")) {
+            const embed = new MessageEmbed()
+                .setDescription("**❌ You don't have access to this command!**")
+                .setColor(colors.Red);
+            return message.channel.send(embed).then((msg) => {
+                if (message.channel.messages.resolve(msg.id)) {
+                    msg.delete({ timeout: 5000 }).catch(console.error);
+                }
+            });
+        }
+
+        if (args.length < 1) {
+            message.channel.send("**❌ Please provide a valid URL**");
+        } else {
+            const client = new JWT({
+                email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+                key: process.env.GOOGLE_PRIVATE_KEY,
+                scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+                subject: null,
+            });
+            const sheets = google.sheets({
+                version: "v4",
+                auth: client,
+            });
+            const url = new URL(args.join(""));
+            const regex = /([^spreadsheets?:\/\s])([^\/\s]+)([^\/edit\s])/g;
+            const spreadsheetID = url.pathname.match(regex).join("");
+            const res = await sheets.spreadsheets.values.get({
+                spreadsheetId: spreadsheetID,
+                range: "A1:Z",
+            });
+            console.log(res.data.values);
+        }
     },
 };
