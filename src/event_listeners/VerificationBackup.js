888489caf7b9deca7a5cda03d06f9eded966fc57
Kaaiitoo <paolo.santos_12@yahoo.com.ph>
2021-02-02 18:12:56 +0800

    disabled verication backup

--- src/event_listeners/VerificationBackup.js
+++ src/event_listeners/VerificationBackup.js
@@ -4,58 +4,52 @@ const colors = require("../utils/colors.json");
 
 module.exports = (bot) => {
     bot.on("messageReactionAdd", async (reaction, user) => {
-        const carlbot = reaction.message.guild.roles.cache
-            .find((role) => role.name === "BOTS")
-            .members.find((m) => m.user.username === "Carl-bot");
-
-        // Checks if carl bot is offline
-        if (carlbot.presence.status === "offline") {
-            const logChannel = await bot.channels.cache.get(
-                "694217906296455188"
-            );
-            const verificationChannel = await bot.channels.cache.get(
-                "694572542236688437"
-            );
-            const verificationMessage = await verificationChannel.messages.fetch(
-                "737708438456369293"
-            );
-
-            if (reaction.partial) {
-                try {
-                    await reaction.fetch();
-                } catch (err) {
-                    console.log(
-                        "Something went wrong while fetching the message.",
-                        err
-                    );
-                }
-            }
-
-            const member = reaction.message.guild.member(user);
-            const role = member.guild.roles.cache.find(
-                (role) => role.name === "Q-tizens"
-            );
-
-            if (
-                reaction.emoji.name === "✅" &&
-                reaction.message.content === verificationMessage.content
-            ) {
-                if (member.roles.cache.has(role.id)) {
-                    user.send(`You are already a Q-tizen!`);
-                    return;
-                }
-                await member.roles.add(role);
-
-                const lEmbed = new MessageEmbed()
-                    .setTitle("New Q-tizen!")
-                    .setColor(colors.Green)
-                    .setThumbnail(member.user.displayAvatarURL())
-                    .setDescription(
-                        `**${member.displayName}** has became and official member of the Q-Den Family!`
-                    );
-
-                logChannel.send(lEmbed);
-            }
-        }
+        // const carlbot = reaction.message.guild.roles.cache
+        //     .find((role) => role.name === "BOTS")
+        //     .members.find((m) => m.user.username === "Carl-bot");
+        // // Checks if carl bot is offline
+        // if (carlbot.presence.status === "offline") {
+        //     const logChannel = await bot.channels.cache.get(
+        //         "694217906296455188"
+        //     );
+        //     const verificationChannel = await bot.channels.cache.get(
+        //         "694572542236688437"
+        //     );
+        //     const verificationMessage = await verificationChannel.messages.fetch(
+        //         "737708438456369293"
+        //     );
+        //     if (reaction.partial) {
+        //         try {
+        //             await reaction.fetch();
+        //         } catch (err) {
+        //             console.log(
+        //                 "Something went wrong while fetching the message.",
+        //                 err
+        //             );
+        //         }
+        //     }
+        //     const member = reaction.message.guild.member(user);
+        //     const role = member.guild.roles.cache.find(
+        //         (role) => role.name === "Q-tizens"
+        //     );
+        //     if (
+        //         reaction.emoji.name === "✅" &&
+        //         reaction.message.content === verificationMessage.content
+        //     ) {
+        //         if (member.roles.cache.has(role.id)) {
+        //             user.send(`You are already a Q-tizen!`);
+        //             return;
+        //         }
+        //         await member.roles.add(role);
+        //         const lEmbed = new MessageEmbed()
+        //             .setTitle("New Q-tizen!")
+        //             .setColor(colors.Green)
+        //             .setThumbnail(member.user.displayAvatarURL())
+        //             .setDescription(
+        //                 `**${member.displayName}** has became and official member of the Q-Den Family!`
+        //             );
+        //         logChannel.send(lEmbed);
+        //     }
+        // }
     });
 };
